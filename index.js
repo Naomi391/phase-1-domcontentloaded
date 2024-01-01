ef3c05eb9bad61e8c3f8041efbea9919da1c9ecb
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-01-02 01:38:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", cb);
+
+function cb() {
+  const theText = document.getElementById("text");
+  theText.textContent = "This is really cool!";
+}
